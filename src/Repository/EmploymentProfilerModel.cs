c7edb3880b6be5770e6af257c67074160d2cc363
Ray Santucci <rtucci10@yahoo.com>
2012-01-07 17:03:24 -0500

    Adding EP repository and model layer

---
+++ src/Repository/EmploymentProfilerModel.cs
@@ -0,0 +1,431 @@
+
+using System;
+using System.Collections.Generic;
+using System.Data.Objects;
+using System.Linq;
+using System.Text;
+using System.Linq.Expressions;
+
+namespace RS.EmploymentProfiler.Repository
+{
+    public interface IRepository<T> where T : class
+    {    
+		#region	Methods
+	
+        T GetById(int id);
+        IEnumerable<T> GetAll();
+        IEnumerable<T> Query(Expression<Func<T, bool>> filter);        
+        T Add(T entity);
+        void Remove(T entity);   
+		
+		#endregion
+    }
+	
+	public abstract class Repository<T> : IRepository<T>
+                                  where T : class
+    {
+    	#region Members
+
+    	protected IObjectSet<T> _objectSet;
+        private ObjectContext _context;
+
+    	#endregion
+
+    	#region Ctor
+
+        public Repository(ObjectContext context)
+    	{
+            _context = context;
+            _objectSet = context.CreateObjectSet<T>();
+    	}
+
+        public Repository() 
+        {
+            _context = new EmploymentProfilerDB();
+            _objectSet = _context.CreateObjectSet<T>();
+        }
+
+    	#endregion
+
+    	#region IRepository<T> Members
+
+    	public IEnumerable<T> GetAll()
+    	{
+      		return _objectSet;
+    	}
+
+    	public abstract T GetById(int id);
+
+    	public IEnumerable<T> Query(Expression<Func<T, bool>> filter)
+    	{
+      		return _objectSet.Where(filter);
+    	}
+
+    	public T Add(T entity)
+    	{
+     	 	//_objectSet.AddObject(entity);           
+            _objectSet.AddObject(entity);
+            _context.SaveChanges();
+            return entity;
+    	}
+
+    	public void Remove(T entity)
+    	{
+      		_objectSet.DeleteObject(entity);
+    	}
+
+    	#endregion
+  	}
+
+	
+	public partial class AccomplishmentRepository : Repository<Accomplishment>
+	{
+		#region Ctor
+
+		public AccomplishmentRepository(ObjectContext context)
+   			: base(context)
+		{
+		}
+
+		#endregion
+
+		#region Methods
+ 
+		public override Accomplishment GetById(int id)   
+		{
+			return _objectSet.SingleOrDefault(e => e.Id == id);
+		}
+
+		#endregion		
+	}
+	
+	public partial class AddressRepository : Repository<Address>
+	{
+		#region Ctor
+
+		public AddressRepository(ObjectContext context)
+   			: base(context)
+		{
+		}
+
+		#endregion
+
+		#region Methods
+ 
+		public override Address GetById(int id)   
+		{
+			return _objectSet.SingleOrDefault(e => e.Id == id);
+		}
+
+		#endregion		
+	}
+	
+	public partial class CompanyRepository : Repository<Company>
+	{
+		#region Ctor
+
+		public CompanyRepository(ObjectContext context)
+   			: base(context)
+		{
+		}
+
+		#endregion
+
+		#region Methods
+ 
+		public override Company GetById(int id)   
+		{
+			return _objectSet.SingleOrDefault(e => e.Id == id);
+		}
+
+		#endregion		
+	}
+	
+	public partial class PersonRepository : Repository<Person>
+	{
+		#region Ctor
+
+		public PersonRepository(ObjectContext context)
+   			: base(context)
+		{
+		}
+
+        public PersonRepository() { }
+        
+		#endregion
+
+		#region Methods
+ 
+		public override Person GetById(int id)   
+		{
+			return _objectSet.SingleOrDefault(e => e.Id == id);
+		}
+
+		#endregion		
+	}
+	
+	public partial class PersonCompanyRepository : Repository<PersonCompany>
+	{
+		#region Ctor
+
+		public PersonCompanyRepository(ObjectContext context)
+   			: base(context)
+		{
+		}
+
+		#endregion
+
+		#region Methods
+ 
+		public override PersonCompany GetById(int id)   
+		{
+			return _objectSet.SingleOrDefault(e => e.People_Id == id);
+		}
+
+		#endregion		
+	}
+	
+	public partial class PortfolioItemRepository : Repository<PortfolioItem>
+	{
+		#region Ctor
+
+		public PortfolioItemRepository(ObjectContext context)
+   			: base(context)
+		{
+		}
+
+		#endregion
+
+		#region Methods
+ 
+		public override PortfolioItem GetById(int id)   
+		{
+			return _objectSet.SingleOrDefault(e => e.Id == id);
+		}
+
+		#endregion		
+	}
+	
+	public partial class PortfolioItemTypeRepository : Repository<PortfolioItemType>
+	{
+		#region Ctor
+
+		public PortfolioItemTypeRepository(ObjectContext context)
+   			: base(context)
+		{
+		}
+
+		#endregion
+
+		#region Methods
+ 
+		public override PortfolioItemType GetById(int id)   
+		{
+			return _objectSet.SingleOrDefault(e => e.Id == id);
+		}
+
+		#endregion		
+	}
+	
+	public partial class SkillRepository : Repository<Skill>
+	{
+		#region Ctor
+
+		public SkillRepository(ObjectContext context)
+   			: base(context)
+		{
+		}
+
+		#endregion
+
+		#region Methods
+ 
+		public override Skill GetById(int id)   
+		{
+			return _objectSet.SingleOrDefault(e => e.Id == id);
+		}
+
+		#endregion		
+	}
+	
+	public partial class SkillTypeRepository : Repository<SkillType>
+	{
+		#region Ctor
+
+		public SkillTypeRepository(ObjectContext context)
+   			: base(context)
+		{
+		}
+
+		#endregion
+
+		#region Methods
+ 
+		public override SkillType GetById(int id)   
+		{
+			return _objectSet.SingleOrDefault(e => e.Id == id);
+		}
+
+		#endregion		
+	}
+		
+  public interface IUnitOfWork
+  {
+  	#region	Methods
+	
+			IRepository<Accomplishment> Accomplishments { get; }   
+			IRepository<Address> Addresses { get; }   
+			IRepository<Company> Companies { get; }   
+			IRepository<Person> People { get; }   
+			IRepository<PersonCompany> PersonCompanies { get; }   
+			IRepository<PortfolioItem> PortfolioItems { get; }   
+			IRepository<PortfolioItemType> PortfolioItemTypes { get; }   
+			IRepository<Skill> Skills { get; }   
+			IRepository<SkillType> SkillTypes { get; }   
+	    void Commit();
+	
+	#endregion
+  }
+
+  public partial class UnitOfWork : IUnitOfWork
+  {
+    #region Members
+
+    private readonly ObjectContext _context;
+		private AccomplishmentRepository _accomplishments;
+		private AddressRepository _addresses;
+		private CompanyRepository _companies;
+		private PersonRepository _people;
+		private PersonCompanyRepository _personcompanies;
+		private PortfolioItemRepository _portfolioitems;
+		private PortfolioItemTypeRepository _portfolioitemtypes;
+		private SkillRepository _skills;
+		private SkillTypeRepository _skilltypes;
+	    
+    #endregion
+
+    #region Ctor
+
+    public UnitOfWork(ObjectContext context)
+    {
+      if (context == null)
+      {
+        throw new ArgumentNullException("context wasn't supplied");
+      }
+
+      _context = context;
+    }
+
+    #endregion
+
+    #region IUnitOfWork Members
+
+		public IRepository<Accomplishment> Accomplishments
+	{
+		get
+		{
+			if (_accomplishments == null)
+			{
+				_accomplishments = new AccomplishmentRepository(_context);
+			}
+			return _accomplishments;
+		}
+	}
+		public IRepository<Address> Addresses
+	{
+		get
+		{
+			if (_addresses == null)
+			{
+				_addresses = new AddressRepository(_context);
+			}
+			return _addresses;
+		}
+	}
+		public IRepository<Company> Companies
+	{
+		get
+		{
+			if (_companies == null)
+			{
+				_companies = new CompanyRepository(_context);
+			}
+			return _companies;
+		}
+	}
+		public IRepository<Person> People
+	{
+		get
+		{
+			if (_people == null)
+			{
+				_people = new PersonRepository(_context);
+			}
+			return _people;
+		}
+	}
+		public IRepository<PersonCompany> PersonCompanies
+	{
+		get
+		{
+			if (_personcompanies == null)
+			{
+				_personcompanies = new PersonCompanyRepository(_context);
+			}
+			return _personcompanies;
+		}
+	}
+		public IRepository<PortfolioItem> PortfolioItems
+	{
+		get
+		{
+			if (_portfolioitems == null)
+			{
+				_portfolioitems = new PortfolioItemRepository(_context);
+			}
+			return _portfolioitems;
+		}
+	}
+		public IRepository<PortfolioItemType> PortfolioItemTypes
+	{
+		get
+		{
+			if (_portfolioitemtypes == null)
+			{
+				_portfolioitemtypes = new PortfolioItemTypeRepository(_context);
+			}
+			return _portfolioitemtypes;
+		}
+	}
+		public IRepository<Skill> Skills
+	{
+		get
+		{
+			if (_skills == null)
+			{
+				_skills = new SkillRepository(_context);
+			}
+			return _skills;
+		}
+	}
+		public IRepository<SkillType> SkillTypes
+	{
+		get
+		{
+			if (_skilltypes == null)
+			{
+				_skilltypes = new SkillTypeRepository(_context);
+			}
+			return _skilltypes;
+		}
+	}
+	    
+	
+    public void Commit()
+    {
+      _context.SaveChanges();
+    }
+
+    #endregion
+  }
+}
---
+++ src/Test/Repository/PersonRepositoryTest.cs
@@ -0,0 +1,86 @@
+﻿using RS.EmploymentProfiler.Repository;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Data.Objects;
+using Moq;
+
+
+namespace RS.EmploymentProfiler.Test
+{
+    /// <summary>
+    ///This is a test class for PersonRepositoryTest and is intended
+    ///to contain all PersonRepositoryTest Unit Tests
+    ///</summary>
+    [TestClass()]
+    public class PersonRepositoryTest
+    {
+        private Mock<PersonRepository> mockRepository;
+        private EmploymentProfilerDB context;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            mockRepository = new Mock<PersonRepository>();           
+            context = new EmploymentProfilerDB();
+        }
+
+        [TestCleanup]
+        public void TearDown()
+        {
+            context.Dispose();           
+        }       
+
+        [TestMethod]
+        public void AddPerson()
+        {
+            var person = Person.CreatePerson(-1, "Test First", "Test Last", 21);
+
+            // Act
+            //var result = mockRepository.Object.Add(person);
+            PersonRepository target = new PersonRepository(context);
+            var result = target.Add(person);
+
+            // Assert
+            Assert.AreEqual(person, result);
+        }
+
+        [TestMethod]
+        public void AddPersonMock()
+        {
+            var person = Person.CreatePerson(-1, "Test First", "Test Last", 21);
+
+            // Act
+            var result = mockRepository.Object.Add(person);           
+
+            // Assert
+            Assert.AreEqual(person, result);
+        }
+
+        /// <summary>
+        ///A test for PersonRepository Constructor
+        ///</summary>
+        [TestMethod()]
+        public void PersonRepositoryConstructorTest()
+        {
+            ObjectContext context = null; // TODO: Initialize to an appropriate value
+            PersonRepository target = new PersonRepository(context);
+            Assert.Inconclusive("TODO: Implement code to verify target");
+        }
+
+        /// <summary>
+        ///A test for GetById
+        ///</summary>
+        [TestMethod()]
+        public void GetByIdTest()
+        {
+            ObjectContext context = null; // TODO: Initialize to an appropriate value
+            PersonRepository target = new PersonRepository(context); // TODO: Initialize to an appropriate value
+            int id = 0; // TODO: Initialize to an appropriate value
+            Person expected = null; // TODO: Initialize to an appropriate value
+            Person actual;
+            actual = target.GetById(id);
+            Assert.AreEqual(expected, actual);
+            Assert.Inconclusive("Verify the correctness of this test method.");
+        }
+    }
+}
